45eb9649de96c65668e1e4eb2da2e312f1c6e194
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-01-24 18:29:45 +0100

    fix variable names

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import { useState, useEffect } from "react";
 
 function App() {
 	const [movies, setMovies] = useState([]);
-	const [genre, setGenre] = useState("27");
-	const [year, setYear] = useState(1980);
-	const [decade, setDecade] = useState(1989);
+	const [genre, setGenre] = useState("18");
+	const [startYear, setStartYear] = useState(1960);
+	const [endYear, setEndYear] = useState(1969);
 	const [watchProvider, setWatchProvider] = useState("8");
 	const [isLoading, setIsLoading] = useState(true);
 	const [isError, setIsError] = useState(false);
-	const url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1&primary_release_date.gte=${year}&primary_release_date.lte=${decade}&with_genres=${genre}&with_watch_providers=${watchProvider}&watch_region=DE&with_watch_monetization_types=flatrate`;
+	const url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1&primary_release_date.gte=${startYear}&primary_release_date.lte=${endYear}&with_genres=${genre}&with_watch_providers=${watchProvider}&watch_region=DE&with_watch_monetization_types=flatrate`;
 
 	const loadMovies = () => {
 		setIsError(false);
@@ -26,19 +26,19 @@ function App() {
 			.catch((err) => setIsError(true));
 	};
 
-	useEffect(loadMovies, [genre, watchProvider, decade]);
+	useEffect(loadMovies, [genre, watchProvider, endYear]);
 
 	useEffect(() => {
-		const startYear = parseInt(year);
-		const endYear = startYear + 10;
-		setDecade(endYear);
-	}, [year]);
+		const firstYear = parseInt(startYear);
+		const secondYear = firstYear + 9;
+		setEndYear(secondYear);
+	}, [startYear]);
 
 	console.log(movies);
 	console.log(`genre id is ${genre}`);
-	console.log(`watch povider id is ${watchProvider}`);
-	console.log(`start year is ${year}`);
-	console.log(`end year is ${decade}`);
+	console.log(`watch provider id is ${watchProvider}`);
+	console.log(`start year is ${startYear}`);
+	console.log(`end year is ${endYear}`);
 	console.log(`url is ${url}`);
 
 	const getContent = () => {
@@ -67,16 +67,22 @@ function App() {
 					name="genreSelect"
 					onChange={(e) => setGenre(e.currentTarget.value)}
 				>
-					<option value="27">horror</option>
-					<option value="28">action</option>
+					<option value="18">drama</option>
 					<option value="35">comedy</option>
 					<option value="53">thriller</option>
+					<option value="12">adventure</option>
+					<option value="878">sci-fi</option>
+					<option value="14">fantasy</option>
+					<option value="27">horror</option>
+					<option value="28">action</option>
 				</select>
 				<p>choose a decade</p>
 				<select
 					name="yearSelect"
-					onChange={(e) => setYear(e.currentTarget.value)}
+					onChange={(e) => setStartYear(e.currentTarget.value)}
 				>
+					<option value="1960">1960's</option>
+					<option value="1970">1970's</option>
 					<option value="1980">1980's</option>
 					<option value="1990">1990's</option>
 					<option value="2000">2000's</option>
@@ -87,9 +93,9 @@ function App() {
 					name="serviceSelect"
 					onChange={(e) => setWatchProvider(e.currentTarget.value)}
 				>
-					<option value="8">Netflix</option>
-					<option value="337">Disney +</option>
-					<option value="9">Amazon Prime</option>
+					<option value="8">netflix</option>
+					<option value="337">disney+</option>
+					<option value="9">amazon prime</option>
 				</select>
 				{/* <br />
 				<button>submit</button> */}
