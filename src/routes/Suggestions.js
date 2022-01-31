9de337549167cacdd0b60041479d230244a0d277
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-01-31 15:41:14 +0100

    update suggestion page layout

--- src/routes/Suggestions.js
+++ src/routes/Suggestions.js
@@ -1,29 +1,27 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import useResults from '../components/useResults';
+import useResults from "../components/useResults";
 
-import MovieList from '../components/MovieList';
+import MovieList from "../components/MovieList";
 
-import face from '../images/face.png'
+import face from "../images/face.png";
 
 export default function Suggestions() {
-    const {genreList,startYear,endYear,region,providerList} = useParams();
-    const {loadMovies, isLoading, isError, movies} = useResults()
-	
-    useEffect(() => {
-        loadMovies(
-            startYear, 
-            endYear, 
-            genreList, 
-            providerList, 
-            region	
-            )
-    },[])
+	const { genreList, startYear, endYear, region, providerList } = useParams();
+	const { loadMovies, isLoading, isError, movies } = useResults();
 
-    return ( 
-        <>
-            {movies && <MovieList movies={movies}  watchProvider={providerList} />}
-            <img src={face} className="absolute bottom-3/4 left-[65%] my-auto h-40" alt="cage face" /> 
-        </>
-        );
+	useEffect(() => {
+		loadMovies(startYear, endYear, genreList, providerList, region);
+	}, []);
+
+	return (
+		<>
+			{movies && <MovieList movies={movies} watchProvider={providerList} />}
+			<img
+				src={face}
+				className="absolute bottom-3/4 left-[68%] my-auto h-36"
+				alt="cage face"
+			/>
+		</>
+	);
 }
