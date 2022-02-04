1db323f85aff178d7c2a3ea95e15fea8784fd35a
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-02-04 16:06:19 +0100

    minor styling updates

--- src/routes/Error.js
+++ src/routes/Error.js
@@ -4,8 +4,12 @@ export default function Error() {
 	return (
 		<>
 			<div className="w-full">
-				<img src={face} className="Load-spinner mx-auto h-40" alt="cage face" />
-				<p className="mx-auto font-medium mt-3 text-center text-white">
+				<img
+					src={face}
+					className="Load-spinner mx-auto mt-12 h-40"
+					alt="cage face"
+				/>
+				<p className="mx-auto font-medium mt-9 text-xl text-center text-white">
 					error, try again!
 				</p>
 			</div>
