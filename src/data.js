baecb985fa73cc8f487cf03dc94f56521df69a12
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-02-06 19:01:51 +0100

    update profile layout

--- src/data.js
+++ src/data.js
@@ -15,7 +15,7 @@ import forest from "./images/forest_edit.jpg";
 import gonegirl from "./images/gonegirl_edit.jpg";
 import lambs from "./images/lambs_edit.jpg";
 import mary from "./images/mary_edit.png";
-import noface from "./images/noface_edit2.png";
+import noface from "./images/noface_edit4.jpg";
 import mountain from "./images/mountain_edit.jpg";
 import rafiki from "./images/rafiki_edit.jpg";
 import spaceship from "./images/spaceship_edit.jpg";
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -2,105 +2,142 @@ import { useState } from "react";
 import face from "../images/face.png";
 import { useParams } from "react-router-dom";
 
-
-const moviesTemplate = ["movie 1", "movie 2", "movie 3"]
-const listsTemplate = [{name: "List 1", tags:["tag 1","tag 2"]}, {name: "List 2", tags:["tag 1 ","tag 2 "]}, {name: "List 3", tags:["tag 1","tag 2"]}]
+const moviesTemplate = ["movie 1", "movie 2", "movie 3", "movie 4"];
+const listsTemplate = [
+	{ name: "List 1", tags: ["tag 1", "tag 2"] },
+	{ name: "List 2", tags: ["tag 1 ", "tag 2 "] },
+	{ name: "List 3", tags: ["tag 1", "tag 2"] },
+];
 const user = {
-        "_id": "61fbc7efd3d53fbb96ff69da",
-        "userName": "User Name",
-        "modified": "2022-02-03T12:17:51.056Z",
-        "watchlist": [
-            "354912",
-            "354912",
-            "354912",
-            "354912"
-        ],
-        "privateLists": [
-            "List 1",
-            "List 2",
-            "List 3"
-        ],
-        "publicLists": [
-            "Public List 1",
-            "Public List 2",
-            "Pulic List 3"
-        ],
-        "blacklist": [
-            "512195",
-            "512195",
-            "512195",
-            "512195"
-        ],
-        "__v": 14
-}
-
+	_id: "61fbc7efd3d53fbb96ff69da",
+	userName: "nick_cage64",
+	modified: "2022-02-03T12:17:51.056Z",
+	watchlist: ["354912", "354912", "354912", "354912"],
+	privateLists: [
+		"my first private list",
+		"my second private list",
+		"my third private list",
+	],
+	publicLists: [
+		"my first public list",
+		"my second public list",
+		"my third public list",
+	],
+	blacklist: ["512195", "512195", "512195", "512195"],
+	__v: 14,
+};
 
 export default function Profile() {
+	const [isListActive, setIsListActive] = useState(false);
+	//const {user} = useParams();
 
-const [isListActive,setIsListActive] = useState(false)
-//const {user} = useParams();
+	return (
+		<>
+			<h1 className="text-2xl font-medium text-white text-left mt-12   ">
+				profile
+			</h1>
+			<div className="flex flex-col justify-center w-[180px] mt-6 border-b-2">
+				<div className="flex justify-center w-full">
+					<img className="h-40" src={face} alt="user avatar" />
+				</div>
+				<div>
+					<h1 className="text-lg font-normal text-white text-center mt-3">
+						{" "}
+						{user.userName}{" "}
+					</h1>
 
-return <>
-    <h1 className="text-2xl font-bold text-white text-left mt-10"> {user.userName} </h1>
-
-    <div className="flex justify-start	items-center mt-5">
-            <img
-                className="
-                h-40 w-40
-                object-contain 
-                "
-                src={ face}
-                alt="user avatar"
-            />    
-            <div className="flex-column  mx-auto mb-4">
-                <ul className="mb-4">
-                    <li  className="text-lg font-medium text-white">So many playlists</li>
-                    <li className="text-lg font-medium text-white">So many wanthced</li>
-                    <li className="text-lg font-medium text-white">I like this and that</li>
-                </ul>
-                <button className="btn-primary">Edit User</button>
-            </div>
-    </div>
-    <h1 className="text-xl font-bold text-white mt-10 mb-4 text-left">Movie lists</h1>
-
-    <div className="flex items-start justify-between">
-        <div className="w-full max-w-xs mr-5">
-            {/* shadow for the div below: shadow-md  */}
-            
-            <div className="bg-white rounded-xl px-8 pt-6 pb-8 mb-4 bg-opacity-0 items-start">
-                <div className="mb-4" >
-                    <div
-                        className="
-                        hover:scale-110 hover:outline-4 
-                        hover:outline-slate-400/40 hover:outline 
-                        hover:outline-offset-0 hover:shadow-xl 
-                        transition-all duration-700 \
-                        shadow-lg 
-                        max-w-2xl 
-                        min-h-[110px] 
-                        bg-black bg-opacity-40  
-                        pr-1 
-                        rounded-r-full 
-                        rounded-l-[1800px] 
-                        flex justify-begin content-center
-                        space-x-4 
-                        overflow-hidden
-                        "
-                        onClick={()=>setIsListActive(!isListActive)}
-                        >
-                        <h1 className={"text-xl font-bold text-white my-auto ml-5 "}>  Watchlist </h1>
-                        <h2 className={"text-md font-medium text-white my-auto"}> Your presonal watchlist </h2>
-                    </div>
-                </div>
-                    <div className="mb-4" >
-                        <div
-                            className="
+					<p className="text-md font-medium text-white mt-2">
+						"my type is dark fairy"
+					</p>
+					<button className="btn-primary mt-4 mb-8">edit</button>
+				</div>
+			</div>
+			<h1 className="text-2xl font-medium text-white text-left mt-8">
+				playlists
+			</h1>
+			<div className="flex flex-col sm:flex-row">
+				<div className="flex flex-col">
+					<div>
+						<h1 className="text-lg font-medium text-white mt-6 mb-3 ml-1 text-left">
+							main lists
+						</h1>
+						<div className="flex flex-col justify-left">
+							<button
+								className="btn-primary mb-2 mr-5 w-44"
+								onClick={() => setIsListActive(!isListActive)}
+							>
+								watch later
+							</button>
+							<button className="btn-primary mb-2  mr-5 w-44">hidden</button>
+						</div>
+					</div>
+					<div>
+						<h1 className="text-lg font-medium text-white mt-6 mb-3 ml-1 text-left">
+							public lists
+						</h1>
+						<div className="flex flex-col justify-left">
+							{user.publicLists.map((list) => (
+								<button
+									className="btn-primary mb-2 mr-5 w-44 truncate"
+									key={list}
+									onClick={() => setIsListActive(!isListActive)}
+								>
+									{list}
+								</button>
+							))}
+						</div>
+					</div>
+					<div>
+						<h1 className="text-lg font-medium text-white mt-6 mb-3 ml-1  text-left">
+							private lists
+						</h1>
+						<div className="flex flex-col justify-left">
+							{user.privateLists.map((list) => (
+								<button
+									className="btn-primary mb-2 mr-5 w-44 truncate"
+									key={list}
+									onClick={() => setIsListActive(!isListActive)}
+								>
+									{list}
+								</button>
+							))}
+						</div>
+					</div>
+				</div>
+				<div className="px-8 h-[600px] pt-0 pb-8 mt-0 mb-4 border-0 border-white w-full">
+					<div
+						className={
+							isListActive
+								? `transition-all duration-700 w-full`
+								: `transition-all duration-700 hidden`
+						}
+					>
+						<div className="mb-4 ">
+							<h2 className="text-lg font-medium text-white  mb-2 text-center">
+								active list name
+							</h2>
+							<h2 className={"text-base font-medium text-white mb-2 my-auto"}>
+								{" "}
+								description of active list{" "}
+							</h2>
+							<p className=" text-md font-normal text-white text-center">
+								#first_tag #second_tag #third_tag
+							</p>
+						</div>
+						<div className="mb-4 flex items-center justify-around">
+							<button className="btn-primary">edit</button>
+						</div>
+						<div className=" justify-center w-full border-4 border-white h-[390px] overflow-scroll p-4 rounded-xl">
+							{moviesTemplate.map((movie) => (
+								<div className="mb-4 flex justify-center w-full" key={movie}>
+									<div
+										className="
                             hover:scale-110 hover:outline-4 
                             hover:outline-slate-400/40 hover:outline 
                             hover:outline-offset-0 hover:shadow-xl 
                             transition-all duration-700 \
                             shadow-lg 
-                            max-w-2xl 
+                            w-full
                             min-h-[110px] 
                             bg-black bg-opacity-40  
                             pr-1 
@@ -110,114 +147,24 @@ return <>
                             space-x-4 
                             overflow-hidden
                             "
-                            onClick={()=>setIsListActive(!isListActive)}
-                            >
-                            <h1 className={"text-xl font-bold text-white my-auto ml-5 "}>  No show List </h1>
-                            <h2 className={"text-md font-medium text-white my-auto"}> Movies you selected to hidde </h2>
-                        </div>
-                    </div>
-            
-            
-            
-            
-            
-            <h1 className="text-xl font-bold text-white mt-10 mb-4 text-left">Private Lists</h1>
-            {user.privateLists.map((list)=>(
-                    <div className="mb-4" key={list}>
-                        <div
-                            className="
-                            hover:scale-110 hover:outline-4 
-                            hover:outline-slate-400/40 hover:outline 
-                            hover:outline-offset-0 hover:shadow-xl 
-                            transition-all duration-700 \
-                            shadow-lg 
-                            max-w-2xl 
-                            min-h-[110px] 
-                            bg-black bg-opacity-40  
-                            pr-1 
-                            rounded-r-full 
-                            rounded-l-[1800px] 
-                            flex justify-begin content-center
-                            space-x-4 
-                            overflow-hidden
-                            "
-                            onClick={()=>setIsListActive(!isListActive)}
-                            >
-                            <h1 className={"text-xl font-bold text-white my-auto ml-5 "}>  {list} </h1>
-                            <h2 className={"text-lg font-bold text-white my-auto"}> Tag 1, Tag2, Tag 3 </h2>
-                        </div>
-                    </div>
-
-                ))}
-                <h1 className="text-xl font-bold text-white mt-10 mb-4 text-left">Public Lists</h1>
-                {user.publicLists.map((list)=>(
-                    <div className="mb-4" key={list}>
-                        <div
-                            className="
-                            hover:scale-110 hover:outline-4 
-                            hover:outline-slate-400/40 hover:outline 
-                            hover:outline-offset-0 hover:shadow-xl 
-                            transition-all duration-700 \
-                            shadow-lg 
-                            max-w-2xl 
-                            min-h-[110px] 
-                            bg-black bg-opacity-40  
-                            pr-1 
-                            rounded-r-full 
-                            rounded-l-[1800px] 
-                            flex justify-begin content-center
-                            space-x-4 
-                            overflow-hidden
-                            "
-                            onClick={()=>setIsListActive(!isListActive)}
-                            >
-                            <h1 className={"text-xl font-bold text-white my-auto ml-5 "}>  {list} </h1>
-                            <h2 className={"text-lg font-bold text-white my-auto"}> Tag 1, Tag2, Tag 3 </h2>
-                        </div>
-                    </div>
-
-                ))}
-            </div>
-        </div>
-        <div className={isListActive ? `transition-all duration-700 w-full` : `transition-all duration-700 hidden`}  >
-            <div className="bg-white shadow-md rounded-xl px-8 pt-6 pb-8 mb-4 bg-opacity-20">
-                <div className="mb-4 ">
-                <h2 className=" text-xl font-bold text-white text-center">
-                    Active List 
-                </h2>
-                </div>
-                <div className="mb-4 flex items-center justify-around">
-                <p className=" text-md font-medium text-white text-center">Tags: Tag1 Tag2 Tag3</p>
-                <button className="btn-primary">Edit</button>
-                </div>
-                {moviesTemplate.map((movie)=>(
-                    <div className="mb-4" key={movie}>
-                        <div
-                            className="
-                            hover:scale-110 hover:outline-4 
-                            hover:outline-slate-400/40 hover:outline 
-                            hover:outline-offset-0 hover:shadow-xl 
-                            transition-all duration-700 \
-                            shadow-lg 
-                            max-w-2xl 
-                            min-h-[110px] 
-                            bg-black bg-opacity-40  
-                            pr-1 
-                            rounded-r-full 
-                            rounded-l-[1800px] 
-                            flex justify-begin content-center
-                            space-x-4 
-                            overflow-hidden
-                            "
-                            >
-                            <h1 className={"text-xl font-bold text-white my-auto ml-5 "}>  {movie} </h1>
-                            <h2 className={"text-lg font-bold text-white my-auto"}> Info </h2>
-                        </div>
-                    </div>
-
-                ))}
-            </div>
-        </div>
-    </div>
-  </>;
+									>
+										<h1
+											className={"text-xl font-bold text-white my-auto ml-5 "}
+										>
+											{" "}
+											{movie}{" "}
+										</h1>
+										<h2 className={"text-lg font-bold text-white my-auto"}>
+											{" "}
+											Info{" "}
+										</h2>
+									</div>
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
