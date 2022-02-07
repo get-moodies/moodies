2a8070baac689a8e036fa380ebab54476e4b3658
garciamarin <gsosa_97@hotmail.com>
2022-02-07 13:38:47 +0000

    added playlist controllers

--- src/routes/Layout.js
+++ src/routes/Layout.js
@@ -1,9 +1,24 @@
 import Header from "../components/HeaderLayout";
 import { Outlet } from "react-router-dom";
+import useLists from "../components/useLists"
 
 //import RegisterLoginLayout from "../components/RegisterLoginLayout";
 
 export default function Layout() {
+
+const {
+    getPublicLists, 
+    getAllLists, 
+    deleteList, 
+    editList,
+    addList
+} = useLists()
+	
+console.log( 	"public:", getPublicLists("gerardo"), 
+   				"private:", getAllLists("gerardo"))
+    // deleteList(gerardo), 
+    // editList(gerardo),
+    // addList(gerardo))
 	return (
 		<div className="App-main lg:w-[1024px] mx-auto p-5 justify-center text-center w-full">
 			<Header />
