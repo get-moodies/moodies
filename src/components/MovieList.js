660e8bc9d2a91e27db6e2ba5a5afb53361fcb8c1
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-02-08 23:15:23 +0100

    add save ui

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -183,9 +183,7 @@ export default function MovieList({ movies, watchProvider }) {
 												hide
 											</button>
 											<SaveButton />
-											<button className="-mb-10 w-20 mt-3 mr-2 bg-opacity-40 bg-white hover:bg-white hover:bg-opacity-60 px-4 py-2 rounded-full font-medium text-sm outline outline-offset-0 outline-0 outline-white text-slate-900">
-												save
-											</button>
+
 											<Trailer title={title} />
 											<a
 												href={(
