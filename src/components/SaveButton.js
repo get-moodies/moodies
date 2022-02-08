060572ecca13e210cb0dd79880a9dece1b53a424
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-02-08 22:51:28 +0100

    start layout

---
+++ src/components/SaveButton.js
@@ -0,0 +1,7 @@
+export default function SaveButton() {
+	return (
+		<button className="-mb-10 w-20 mt-3 mr-2 bg-opacity-40 bg-white hover:bg-white hover:bg-opacity-60 px-4 py-2 rounded-full font-medium text-sm outline outline-offset-0 outline-0 outline-white text-slate-900">
+			save
+		</button>
+	);
+}
